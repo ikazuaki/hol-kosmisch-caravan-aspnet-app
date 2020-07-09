a8fedcde95c5dc9728f44cab2f9d2f6f630d6a11
井上和昭 <ikazuaki@MacBook-Pro.local>
2020-07-09 15:27:38 +0900

    ASP.NET Coreに移行した

--- MyWebApp/Controllers/HomeController.cs
+++ MyWebApp/Controllers/HomeController.cs
@@ -1,24 +1,37 @@
-﻿using MyWebApp.Models;
-using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyWebApp.Models;
 
 namespace MyWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
         {
-            ViewBag.Message = Session["message"]?.ToString() ?? "";
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
             return View();
         }
 
-        [HttpPost]
-        public ActionResult Index(MyForm item)
+        public IActionResult Privacy()
         {
-            if (!string.IsNullOrWhiteSpace(item?.Message))
-            {
-                Session["message"] = item.Message;
-            }
-            return RedirectToAction("Index");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
